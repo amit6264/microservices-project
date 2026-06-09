3e8de22b306f9b1440be397d6a19881733b77ab2
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-09 12:35:57 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,25 +1,11 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Frontend Service Running AWS Ec2 instance");
-// });
+app.get("/", (req, res) => {
+  res.send("Frontend Service Running AWS Ec2 instance");
+});
 
-// app.listen(3000, () => {
-//   console.log("Frontend running on 3000");
-// });
-
-
-
-function add(a, b) {
-  return a + b;
-}
-
-function addNumbers(a, b) {
-  return a + b;
-}
-
-function sum(a, b) {
-  return a + b;
-}
+app.listen(3000, () => {
+  console.log("Frontend running on 3000");
+});
